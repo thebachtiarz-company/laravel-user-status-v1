a326cdc15a2acce0e84e5f3a5b5e2e57ed65f92f
thebachtiarz <bachtiarptibinlppnri2417@gmail.com>
2022-10-05 01:25:12 +0700

    update - migrations

--- database/migrations/2022_10_20_023020_create_user_statuses_table.php
+++ database/migrations/2022_10_20_023020_create_user_statuses_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use TheBachtiarz\UserStatus\Interfaces\Model\UserStatusModelInterface;
 use TheBachtiarz\UserStatus\Models\StatusUser;
 use TheBachtiarz\UserStatus\Models\User;
 
@@ -17,8 +18,8 @@ return new class extends Migration
     {
         Schema::create('user_statuses', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class)->unique()->nullable()->constrained()->nullOnDelete();
-            $table->foreignIdFor(StatusUser::class)->nullable()->constrained()->nullOnDelete();
+            $table->foreignIdFor(User::class, UserStatusModelInterface::USER_STATUS_ATTRIBUTE_USERID)->unique()->nullable()->constrained()->nullOnDelete();
+            $table->foreignIdFor(StatusUser::class, UserStatusModelInterface::USER_STATUS_ATTRIBUTE_STATUSUSERID)->nullable()->constrained()->nullOnDelete();
             $table->timestamps();
         });
     }
